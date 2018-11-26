c81bfaa60fc2e88dfff0a7925d222751480c7c63
Abdulmajeed Alomari <abdulmajeedmmo@gmail.com>
2018-11-26 19:22:44 +0300

    refactor: Increase readability

--- src/RAM.java
+++ src/RAM.java
@@ -2,91 +2,76 @@ import java.util.*;
 
 public class RAM {
 	private final static int RAM_SIZE = (int) (160*0.9);
-	private int usage;
+	private static int usage;
 	private final static int ADDITIONAL_PROCESS = (int) (160*0.1);
-	private int usageA;
+	private static int usageA;
 //	private static Queue <PCB> jobQ = new PriorityQueue<PCB>(); do we have to create this queue?
 	private static Queue <PCB> readyQ = new PriorityQueue<PCB>();
-	private static Queue <PCB> waitingQ = new PriorityQueue<PCB>();//different from I/O queue
-
-	public boolean isFull(){
-		return usage == RAM_SIZE;
-	}
+	private static Queue <PCB> waitingQ = new PriorityQueue<PCB>(); //different from I/O queue
 	
 	// to add new process
-	public boolean addNewProcess(PCB obj){
-		if(!(usage + obj.getSize()>=RAM_SIZE)){
-		obj.setProcessState(ProcessState.READY);
-		readyQ.add(obj);
-		usage +=obj.getSize();
-		return true;
+	static boolean addNewProcess(PCB process){
+		if(usage + process.getSize() > RAM_SIZE){
+            process.setProcessState(ProcessState.WAITING);
+            waitingQ.add(process);
+            return false;
 		}
-		obj.setProcessState(ProcessState.WAITING);
-		waitingQ.add(obj);
-		return false;
+
+        process.setProcessState(ProcessState.READY);
+        readyQ.add(process);
+        usage +=process.getSize();
+        return true;
 	}
-	
+
+    //TODO Ask Abdulmajeed about this method and its conditions
 	// to add additional CPU for old process
-	public boolean addAdditionalProcess(PCB obj){
-		if(!(usageA+obj.getSize()>=ADDITIONAL_PROCESS)){
-			obj.setProcessState(ProcessState.READY);
-		readyQ.add(obj);
-		return true;
+	static boolean addAdditionalProcess(PCB process){
+		if(usageA+process.getSize() < ADDITIONAL_PROCESS){
+			process.setProcessState(ProcessState.READY);
+            readyQ.add(process);
+            return true;
 		}
-		obj.setProcessState(ProcessState.WAITING);
-		waitingQ.add(obj);
-		return false;
+
+        process.setProcessState(ProcessState.WAITING);
+        waitingQ.add(process);
+        return false;
 	}
 	
-	//to serve from ready queue .. and check to add process form waiting queue .. 
+	// to serve from ready queue .. and check to add process form waiting queue ..
 	// read document point 6
-	public PCB deQueue(){
-		if(!readyQ.isEmpty()){
-			PCB process = readyQ.remove();
-			if(!(usageA-process.getBurstTime()<0)){
-				usageA -=usageA-process.getSize();
-			}else{ 
-				usage -=usage-process.getSize();
-			}
-			if(!(waitingQ.isEmpty())&&(waitingQ.peek().getSize()+usage<=RAM_SIZE)){
-				readyQ.add(waitingQ.remove());
-				
-			}
-			return process;
-		}
-		return null;
+	static PCB deQueue(){
+		if(readyQ.isEmpty())
+		    return null;
+
+        PCB process = readyQ.remove();
+
+        if(usageA-process.getBurstTime() >= 0)
+            usageA -= usageA-process.getSize();
+        else
+            usage -= usage-process.getSize();
+
+        // If there is available space in ram, then add new process to it.
+        if(!waitingQ.isEmpty() && (waitingQ.peek().getSize()+usage<=RAM_SIZE))
+            readyQ.add(waitingQ.remove());
+
+        return process;
 	}
 
-	public int getUsage() {
-		return usage;
-	}
+    static boolean isFull(){
+        return usage == RAM_SIZE;
+    }
 
-	public void setUsage(int usage) {
-		this.usage = usage;
-	}
+	// Getters/Setters
+	public int getUsage() { return usage; }
+	public void setUsage(int usage) { RAM.usage = usage; }
 
-	public int getUsageA() {
-		return usageA;
-	}
+	public int getUsageA() { return usageA; }
+	public void setUsageA(int usageA) { RAM.usageA = usageA; }
 
-	public void setUsageA(int usageA) {
-		this.usageA = usageA;
-	}
+	public static Queue<PCB> getReadyQ() { return readyQ; }
+	public static void setReadyQ(PriorityQueue<PCB> readyQ) { RAM.readyQ = readyQ; }
 
-	public static Queue<PCB> getReadyQ() {
-		return readyQ;
-	}
-
-	public static void setReadyQ(PriorityQueue<PCB> readyQ) {
-		RAM.readyQ = readyQ;
-	}
-
-	public static Queue<PCB> getWaitingQ() {
-		return waitingQ;
-	}
-
-	public static void setWaitingQ(PriorityQueue<PCB> waitingQ) {
-		RAM.waitingQ = waitingQ;
-	}
+	public static Queue<PCB> getWaitingQ() { return waitingQ; }
+	public static void setWaitingQ(PriorityQueue<PCB> waitingQ) { RAM.waitingQ = waitingQ; }
 	
 }
