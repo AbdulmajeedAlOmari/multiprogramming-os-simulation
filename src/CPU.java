eeab9032ac235cdbad23b3b0de687449755fb804
Abdulmajeed Alomari <abdulmajeedmmo@gmail.com>
2018-11-30 20:31:20 +0300

    feat: Handle Process in CPU

--- src/CPU.java
+++ src/CPU.java
@@ -20,27 +20,37 @@ public class CPU extends Thread {
     }
 
     private void handleCurrentProcess() {
+        //Rename the process for simplicity
+        PCB process = this.currentActiveProcess;
+
         // Set its state to Running
-        this.currentActiveProcess.setProcessState(ProcessState.RUNNING);
+        process.setProcessState(ProcessState.RUNNING);
 
         // Get its first burst
-        Burst currentBurst = this.currentActiveProcess.getCurrentBurst();
+        Burst currentBurst = process.getCurrentBurst();
 
         // Check if it is an IO burst or CPU burst
         if(currentBurst.getType().equals(BurstType.IO)) {
-            ioDevice.addProcessToDevice(this.currentActiveProcess);
+            ioDevice.addProcessToDevice(process);
             this.currentActiveProcess = null;
             return;
         }
+
+        //Increment the CPU Counter for this process
+        process.incrementCpuCounter();
 
         // If it is CPU burst then process it
         while(currentBurst.getRemainingTime() > 0) {
             // Decrease the remaining time by 1 ms
             currentBurst.decrementRemainingTime();
 
-            // TODO Handle process logic and hook it with GUI
+            // TODO hook it with GUI
+
+            //Increment the total time it was processed in CPU
+            process.incrementCpuTotalTime();
 
             try {
+                //Wait for 1 millisecond before proceeding
                 sleep(1);
             } catch (InterruptedException e) {
                 e.printStackTrace();
@@ -50,14 +60,15 @@ public class CPU extends Thread {
         // TODO handle processes memory size and manipulate it
 
         // Get the next burst and load it in PCB
-        Burst nextBurst = this.currentActiveProcess.nextBurst();
+        Burst nextBurst = process.nextBurst();
 
         if(nextBurst == null) {
+            // TODO save the time when it was TERMINATED/KILLED
             // This process finished all of its bursts normally
-            this.currentActiveProcess.setProcessState(ProcessState.TERMINATED);
+            process.setProcessState(ProcessState.TERMINATED);
         } else if(nextBurst.getType().equals(BurstType.IO)) {
             // The next burst is IO, so change its state to WAITING
-            this.currentActiveProcess.setProcessState(ProcessState.WAITING);
+            process.setProcessState(ProcessState.WAITING);
             ioDevice.addProcessToDevice(this.currentActiveProcess);
         } else if (nextBurst.getType().equals(BurstType.CPU)) {
             // TODO check if we need this statement
