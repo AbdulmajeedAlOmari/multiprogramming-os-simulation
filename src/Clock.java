1289c90590848dffeb003ffcf3bbce106c1c845a
Abdulmajeed Alomari <abdulmajeedmmo@gmail.com>
2018-12-01 23:24:39 +0300

    feat: Let Clock start the longTermScheduler in RAM

--- src/Clock.java
+++ src/Clock.java
@@ -2,11 +2,23 @@
  * This class will behave like a clock for our Operating System
  * Time will be in Milliseconds
  */
-public class Clock {
+public class Clock extends Thread {
     private long initialTime;
 
     public Clock() {
         this.initialTime = System.currentTimeMillis();
+    }
+
+    @Override
+    public void run() {
+        while(true) {
+            try {
+                RAM.longTermScheduler();
+                sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     int getCurrentMs() {
