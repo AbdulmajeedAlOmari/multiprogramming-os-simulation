6d09bafe324461496c52f0688a5e822935f78763
Abdulmajeed Alomari <abdulmajeedmmo@gmail.com>
2018-12-03 01:15:19 +0300

    refactor: Remove unused public modifiers

--- src/SuperRAM.java
+++ src/SuperRAM.java
@@ -1,26 +1,26 @@
 
 public class SuperRAM extends Thread {
-
 	private final static int RAM_SIZE = (int) (160*0.9);
 	private int usage;
 	private final static int ADDITIONAL_PROCESS = (int) (160*0.1);
 	private int usageA;
-	public int getUsage() {
+
+	int getUsage() {
 		return usage;
 	}
-	public void setUsage(int usage) {
+	void setUsage(int usage) {
 		this.usage = usage;
 	}
-	public int getUsageA() {
+	int getUsageA() {
 		return usageA;
 	}
-	public void setUsageA(int usageA) {
+	void setUsageA(int usageA) {
 		this.usageA = usageA;
 	}
-	public static int getRamSize() {
+	static int getRamSize() {
 		return RAM_SIZE;
 	}
-	public static int getAdditionalProcess() {
+	static int getAdditionalProcess() {
 		return ADDITIONAL_PROCESS;
 	}
 }
