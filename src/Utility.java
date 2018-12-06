639ce871244c44b2588a23dc6c8aaba2c552a60e
Abdulmajeed Alomari <abdulmajeedmmo@gmail.com>
2018-12-06 18:34:40 +0300

    feat: Add utility class

---
+++ src/Utility.java
@@ -0,0 +1,17 @@
+/***
+ * This class will have all global/configurable variables
+ */
+public class Utility {
+
+    // Time in (ms) per CPU clock
+    public static final int TIME = 1;
+
+    // The input file full path
+    public static final String INPUT_FILE_PATH = "";
+
+    // The output file full path
+    public static final String OUTPUT_FILE_PATH = "";
+
+    // Dubug mode?
+    public static final boolean DEBUG_MODE = false;
+}
